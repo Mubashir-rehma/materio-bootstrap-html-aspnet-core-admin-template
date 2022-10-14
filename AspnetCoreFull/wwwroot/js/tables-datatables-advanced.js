ba7f2e43e00e54f52ef97a8e0b37f9230362f25d
monab <monajmakwana@gmail.com>
2022-10-14 12:34:43 +0530

    fix(multi): pages, assets and editorconfig added editor config updated dist files of updated js urls updated pages as per CRLF

--- AspnetCoreFull/wwwroot/js/tables-datatables-advanced.js
+++ AspnetCoreFull/wwwroot/js/tables-datatables-advanced.js
@@ -96,7 +96,7 @@ $(function () {
   if (dt_ajax_table.length) {
     var dt_ajax = dt_ajax_table.dataTable({
       processing: true,
-      ajax: assetsPath + 'json/ajax.php',
+      ajax: assetsPath + 'json/ajax.json',
       dom: '<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6 d-flex justify-content-center justify-content-md-end"f>><"table-responsive"t><"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>'
     });
   }
