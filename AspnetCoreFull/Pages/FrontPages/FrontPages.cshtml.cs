03f277895fc8e51722ede478ad27152e30ff71a9
monab <monajmakwana@gmail.com>
2023-08-02 11:30:12 +0530

    feat(layout): added new front pages layouts updated common file structure

---
+++ AspnetCoreFull/Pages/FrontPages/FrontPages.cshtml.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetCoreFull.Pages.FrontPages
+{
+  public class LandingPageModel : PageModel
+  {
+    public void OnGet() { }
+  }
+}
