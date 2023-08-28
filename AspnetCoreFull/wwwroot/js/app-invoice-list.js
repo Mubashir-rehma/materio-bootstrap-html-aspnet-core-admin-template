b172cb438acba69f6cbef6972f7a2168b30ebe75
SHVM-09 <monkeydeluffy98@icloud.com>
2023-08-28 18:16:18 +0530

    fix(multi): minor fixes in cshtml & js as per https://github.com/themeselection/sneat-bootstrap-html-admin-template-dev/issues/31

--- AspnetCoreFull/wwwroot/js/app-invoice-list.js
+++ AspnetCoreFull/wwwroot/js/app-invoice-list.js
@@ -190,9 +190,9 @@ $(function () {
       ],
       order: [[1, 'desc']],
       dom:
-        '<"row ms-2 me-3"' +
-        '<"col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-start gap-2"l<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start mt-md-0 mt-3"B>>' +
-        '<"col-12 col-md-6 d-flex align-items-center justify-content-end flex-column flex-md-row pe-3 gap-md-2"f<"invoice_status mb-3 mb-md-0">>' +
+        '<"row mx-1"' +
+        '<"col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-start gap-3"l<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start mt-md-0 mt-3"B>>' +
+        '<"col-12 col-md-6 d-flex align-items-center justify-content-end flex-column flex-md-row pe-3 gap-md-3"f<"invoice_status mb-3 mb-md-0">>' +
         '>t' +
         '<"row mx-2"' +
         '<"col-sm-12 col-md-6"i>' +
