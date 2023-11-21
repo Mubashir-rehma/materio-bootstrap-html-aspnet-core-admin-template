5b159edd1be5e99ef2a9108f11df506d5788a97d
SHVM-09 <monkeydeluffy98@icloud.com>
2023-11-21 13:50:20 +0530

    feat(multi): migrate to .net 8 - fixes in locales - updated docker files for .net 8 compatibility - removed all terminal warnings - updated gitignore

--- AspnetCoreFull/Models/User.cs
+++ AspnetCoreFull/Models/User.cs
@@ -7,23 +7,23 @@ namespace AspnetCoreFull.Models
   public class User
   {
     public int Id { get; set; }
-    public string UserName { get; set; }
-    public string Email { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
     public bool IsVerified { get; set; }
-    public string ContactNumber { get; set; }
+    public string? ContactNumber { get; set; }
 
     // Role selection (Choose one role from the list)
-    public string SelectedRole { get; set; }
+    public string? SelectedRole { get; set; }
 
     // Exclude AvailablePlans from database modeling
     [NotMapped]
-    public List<string> AvailableRoles { get; set; }
+    public List<string>? AvailableRoles { get; set; }
 
     // Plan selection (Choose one plan from the list)
-    public string SelectedPlan { get; set; }
+    public string? SelectedPlan { get; set; }
 
     // Exclude AvailablePlans from database modeling
     [NotMapped]
-    public List<string> AvailablePlans { get; set; }
+    public List<string>? AvailablePlans { get; set; }
   }
 }
